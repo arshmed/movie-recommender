1e47c6e3a2f9dd003f5b4f48e35865aeaf36f448
mehmet-can-yakit <109636142+mehmet-can-yakit@users.noreply.github.com>
2023-01-02 23:37:31 +0300

    auth changes

--- lib/auth/login_home_page.dart
+++ lib/auth/login_home_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:moviee/auth/read%20data/get_user_name.dart';
 
 class HomePageLogin extends StatefulWidget {
   const HomePageLogin({Key key}) : super(key: key);
@@ -9,22 +11,56 @@ class HomePageLogin extends StatefulWidget {
 }
 
 class _HomePageLoginState extends State<HomePageLogin> {
-  final user = FirebaseAuth.instance.currentUser;
+  final currentuser = FirebaseAuth.instance.currentUser;
+
+  // doc ID's
+  List<String> docID = [];
+
+  // method getting docIds
+  Future getDocId() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .where("email", isEqualTo: currentuser.email)
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((document) {
+              //print(document.reference);
+              docID.add(document.reference.id);
+            }));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(currentuser.email),
+        actions: [
+          GestureDetector(
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Icon(Icons.logout))
+        ],
+      ),
       body: Center(
           child: Column(
         children: [
-          Text("Signed in"),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
+          Expanded(
+              child: FutureBuilder(
+            future: getDocId(),
+            builder: (context, snapshot) {
+              return ListView.builder(
+                  itemCount: docID.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        tileColor: Colors.deepOrange,
+                        title: GetUserName(docID[index]),
+                      ),
+                    );
+                  });
             },
-            color: Colors.deepOrange,
-            child: const Text("Sign out!"),
-          )
+          ))
         ],
       )),
     );
